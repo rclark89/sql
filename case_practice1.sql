0ea9a53fb6d2035ef420a96a0e9120dd070671a5
Ross Clark <rossclark89@gmail.com>
2022-01-11 16:52:20 +0000

    Create case_practice1.sql
    Taken from a DataCamp joins practice exercise.

---
+++ case_practice1.sql
@@ -0,0 +1,12 @@
+--Adds column with categories based on size of surface area. 
+SELECT name, continent, code, surface_area,
+    -- First case
+    CASE WHEN surface_area > 2000000 THEN 'large'
+        -- Second case
+        WHEN surface_area > 350000 THEN 'medium'
+        -- Else clause + end
+        ELSE 'small' END
+        -- Alias name
+        AS geosize_group
+-- From table
+FROM countries;
